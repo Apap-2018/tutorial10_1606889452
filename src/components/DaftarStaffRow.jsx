bbca89917143e85a090d1f27f5075dc1ad909165
Rafie Dharmawan <rafiedharmawan@gmail.com>
2018-12-05 20:58:39 +0700

    bismillah

---
+++ src/components/DaftarStaffRow.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+export const DaftarStaffRow = (props) => {
+    return (
+        <tbody>
+            {props.listStaff.map(staff => {
+                return (
+                    <tr key={staff.id}>
+                        <td>{staff.nama}</td>
+                        <td>{staff.jenis}</td>
+                    </tr>
+                )
+            })}
+        </tbody>
+    )
+}
